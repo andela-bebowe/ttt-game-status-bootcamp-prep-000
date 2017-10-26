7709940a1c71f63e4c12fe857b1e900720b914dd
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-26 15:39:43 +0000

    automatically backed up by learn

--- spec/game_status_spec.rb
+++ spec/game_status_spec.rb
@@ -118,32 +118,32 @@ describe "./lib/game_status.rb" do
     end
   end
 
-  # describe '#over?' do
-  #   it 'returns true for a draw' do
-  #     board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-  #
-  #     expect(over?(board)).to be_truthy
-  #   end
-  #
-  #   it 'returns true for a won game when the board is full' do
-  #     board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-  #
-  #     expect(over?(board)).to be_truthy
-  #   end
-  #
-  #   it 'returns true for a won game when the board is not full' do
-  #     board = ["X", " ", " ", "O", "O", "O", "X", "X", " "]
-  #
-  #     expect(over?(board)).to be_truthy
-  #   end
-  #
-  #   it 'returns false for an in-progress game' do
-  #     board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
-  #
-  #     expect(over?(board)).to be_falsey
-  #   end
-  # end
-  #
+  describe '#over?' do
+    it 'returns true for a draw' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      expect(over?(board)).to be_truthy
+    end
+
+    it 'returns true for a won game when the board is full' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
+
+      expect(over?(board)).to be_truthy
+    end
+
+    it 'returns true for a won game when the board is not full' do
+      board = ["X", " ", " ", "O", "O", "O", "X", "X", " "]
+
+      expect(over?(board)).to be_truthy
+    end
+
+    it 'returns false for an in-progress game' do
+      board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+
+      expect(over?(board)).to be_falsey
+    end
+  end
+
   # describe '#winner' do
   #   it 'return X when X won' do
   #     board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
